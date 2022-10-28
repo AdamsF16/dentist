45304813aa59a2f6022b7f61e54446db8ae36b23
Adam <adam_stawiarski7@wp.pl>
2022-10-28 15:49:38 +0200

    desktop design for main offer

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,8 @@
 const navMobileBtn = document.querySelector('.nav__mobile-icon')
 const navMobile = document.querySelectorAll('.nav__item-mobile')
-
+const offerCardOne = document.querySelector(`.offer__card-img--one`)
+const offerCardTwo = document.querySelector(`.offer__card-img--two`)
+const offerCardThree = document.querySelector(`.offer__card-img--three`)
 
 const handleNav = () => {
     navMobile.forEach(link => {
@@ -8,6 +10,19 @@ const handleNav = () => {
     });
 }
 
+const changeImg = () => {
+    if(window.innerWidth >= 992) {
+        offerCardOne.setAttribute(`src`, `../dist/img/dentist-offer-one_1920.jpg`)
+        offerCardTwo.setAttribute(`src`, `../dist/img/dentist-offer-two_1920.jpg`)
+        offerCardThree.setAttribute(`src`, `../dist/img/dentist-offer-three_1920.jpg`)
+    } else {
+        offerCardOne.setAttribute(`src`, `../dist/img/dentist-offer-one_640.jpg`)
+        offerCardTwo.setAttribute(`src`, `../dist/img/dentist-offer-two_640.jpg`)
+        offerCardThree.setAttribute(`src`, `../dist/img/dentist-offer-three_640.jpg`)
+    }
+}
+changeImg()
 
 
 navMobileBtn.addEventListener(`click`, handleNav)
+window.addEventListener(`resize`, changeImg)
