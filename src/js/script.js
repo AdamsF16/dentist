f5500434d1c7f329dc4c19d382be3ae4c64c3960
Adam <adam_stawiarski7@wp.pl>
2022-10-28 17:15:36 +0200

    about us section

--- src/js/script.js
+++ src/js/script.js
@@ -3,6 +3,7 @@ const navMobile = document.querySelectorAll('.nav__item-mobile')
 const offerCardOne = document.querySelector(`.offer__card-img--one`)
 const offerCardTwo = document.querySelector(`.offer__card-img--two`)
 const offerCardThree = document.querySelector(`.offer__card-img--three`)
+const aboutUsImg = document.querySelector(`.about-us__img`)
 
 const handleNav = () => {
     navMobile.forEach(link => {
@@ -19,6 +20,12 @@ const changeImg = () => {
         offerCardOne.setAttribute(`src`, `../dist/img/dentist-offer-one_640.jpg`)
         offerCardTwo.setAttribute(`src`, `../dist/img/dentist-offer-two_640.jpg`)
         offerCardThree.setAttribute(`src`, `../dist/img/dentist-offer-three_640.jpg`)
+    }
+
+    if(window.innerWidth >= 850){
+        aboutUsImg.setAttribute(`src`, `../dist/img/doctor_1920.jpg`)
+    } else {
+        aboutUsImg.setAttribute(`src`, `../dist/img/doctor_640.jpg`)
     }
 }
 changeImg()
