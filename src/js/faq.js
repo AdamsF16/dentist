d469bb6cd48ab3aabbfbd51b7ae3b67d3f07bdbd
Adam <adam_stawiarski7@wp.pl>
2022-12-30 17:07:44 +0100

    faq

--- src/js/faq.js
+++ src/js/faq.js
@@ -5,6 +5,9 @@ const navLanguageOption = document.querySelectorAll(`.nav__language-btn`);
 const navLanguagePolish = document.querySelector(`.nav__language--pl`);
 const navLanguageEnglish = document.querySelector(`.nav__language--eng`);
 const navLanguageContainer = document.querySelector(`.nav__language-btn-container`);
+const answers = document.querySelectorAll(`.faq-box__question-answer`)
+const questionBtn = document.querySelectorAll(`.faq-box__btn`)
+const questionIcon = document.querySelectorAll(`.faq-box__icon`)
 
 
 const handleNav = () => {
@@ -37,13 +40,28 @@ const changeLanguage = (e) => {
     removeLanguageBox()
 };
 
+const showAnswer = (e) => {
+	e.target.closest(`div`).nextElementSibling.classList.toggle(`faq-box__question-answer--visible`)
+	
+	if(e.target.closest(`.faq-box__icon`).classList.contains('fa-angles-down')) {
+	e.target.closest(`.faq-box__icon`).classList.add('fa-angle-up')	
+	e.target.closest(`.faq-box__icon`).classList.remove('fa-angles-down')	
+	} else {
+		e.target.closest(`.faq-box__icon`).classList.add('fa-angles-down')
+		e.target.closest(`.faq-box__icon`).classList.remove('fa-angle-up')
+
+	}
+}
+
+
+const showMoreInfo2 = (e) => {
+	e.target.closest(`.team-card__about-me`).nextElementSibling.classList.toggle('team-card__more--visible')
+}
 
 
 
-
-
-
-
+questionBtn.forEach((btn) => {
+	btn.addEventListener(`click`, showAnswer)})
 navMobileBtn.addEventListener(`click`, handleNav);
 navLanguageBtn.addEventListener(`click`, handleLanguageNav);
 navLanguageContainer.addEventListener(`click`, changeLanguage);
