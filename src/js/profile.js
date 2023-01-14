a5c6b1f99e88e709c15e94056c8028547ba35d3f
Adam <adam_stawiarski7@wp.pl>
2023-01-14 14:13:28 +0100

    profile menu js

--- src/js/profile.js
+++ src/js/profile.js
@@ -5,6 +5,15 @@ const navLanguageOption = document.querySelectorAll(`.nav__language-btn`);
 const navLanguagePolish = document.querySelector(`.nav__language--pl`);
 const navLanguageEnglish = document.querySelector(`.nav__language--eng`);
 const navLanguageContainer = document.querySelector(`.nav__language-btn-container`);
+const myProfileBtn = document.querySelector(`.profile__my-profile-btn`)
+const editBtn = document.querySelector(`.profile__edit-btn`)
+const historyBtn = document.querySelector(`.profile__history-btn`)
+const changePassBtn = document.querySelector(`.profile__change-pass-btn`)
+const myProfile = document.querySelector(`.profile__my-profile-box`)
+const changeData = document.querySelector(`.profile__change-data-box`)
+const visitHistory = document.querySelector(`.profile__visit-history-box`)
+const changePassBox = document.querySelector(`.profile__reset-box`)
+
 
 
 const handleNav = () => {
@@ -39,10 +48,58 @@ const changeLanguage = (e) => {
 
 
 
+const showMyProfile = () => {
+	myProfile.classList.remove(`profile__hidden`);
+	// myProfile.classList.add(`profile__visible`);
+	changeData.classList.remove(`profile__visible`);
+	changeData.classList.add(`profile__hidden`);
+	visitHistory.classList.remove(`profile__visible`);
+	visitHistory.classList.add(`profile__hidden`);
+	changePassBox.classList.remove(`profile__visible`)
+	changePassBox.classList.add(`profile__hidden`)
+}
+
+const showEditPanel = () => {
+	myProfile.classList.remove(`profile__visible`)
+	myProfile.classList.add(`profile__hidden`);
+	changeData.classList.remove(`profile__hidden`);
+	changeData.classList.add(`profile__visible`);
+	visitHistory.classList.remove(`profile__visible`);
+	visitHistory.classList.add(`profile__hidden`);
+	changePassBox.classList.remove(`profile__visible`)
+	changePassBox.classList.add(`profile__hidden`)
+}
+
+const showHistory = () => {
+	myProfile.classList.remove(`profile__visible`)
+	myProfile.classList.add(`profile__hidden`);
+	changeData.classList.remove(`profile__visible`);
+	changeData.classList.add(`profile__hidden`);
+	visitHistory.classList.remove(`profile__hidden`);
+	visitHistory.classList.add(`profile__visible`);
+	changePassBox.classList.remove(`profile__visible`)
+	changePassBox.classList.add(`profile__hidden`)
+}
+
+const showReset = () => {
+	myProfile.classList.remove(`profile__visible`)
+	myProfile.classList.add(`profile__hidden`);
+	changeData.classList.remove(`profile__visible`);
+	changeData.classList.add(`profile__hidden`);
+	visitHistory.classList.remove(`profile__visible`);
+	visitHistory.classList.add(`profile__hidden`);
+	changePassBox.classList.remove(`profile__hidden`)
+	changePassBox.classList.add(`profile__visible`)
+}
 
 
 
 
+
+myProfileBtn.addEventListener(`click`, showMyProfile)
+editBtn.addEventListener(`click`, showEditPanel)
+historyBtn.addEventListener(`click`, showHistory)
+changePassBtn.addEventListener(`click`, showReset)
 
 navMobileBtn.addEventListener(`click`, handleNav);
 navLanguageBtn.addEventListener(`click`, handleLanguageNav);
