ecb13f876a7c7ec8aca926cf2d4d271026b6a7d3
Adam <adam_stawiarski7@wp.pl>
2023-01-14 20:51:00 +0100

    profile reset pass js

--- src/js/profile.js
+++ src/js/profile.js
@@ -18,9 +18,12 @@ const changePassBox = document.querySelector(`.profile__reset-box`)
 const resetPassbtn = document.querySelector(`.profile__reset-btn`)
 const oldPassInput = document.querySelector(`#oldPassword`)
 const newPassInput = document.querySelector(`#changePassword`)
-const repeatPassword = document.querySelector(`#changePasswordRepeat`)
+const repeatPasswordInput = document.querySelector(`#changePasswordRepeat`)
+const allInputs = document.querySelectorAll(`.profile__reset-input`)
 const oldPassAlert = document.querySelector(`.profile__reset-alert--old`)
 const repeatPassAlert = document.querySelector(`.profile__reset-alert--change`)
+const allAlerts = document.querySelectorAll(`.profile__reset-alert`)
+const corectAllert = document.querySelector(`.profile__reset-alert-corect`)
 
 
 
@@ -104,10 +107,66 @@ const showReset = () => {
 
 
 
+
+const checkValue = () => {
+	allInputs.forEach(input => {
+		if(input.value == "") {
+			input.nextElementSibling.classList.add(`profile__reset-alert--visible`)
+			input.nextElementSibling.textContent = `Podane pole nie może być puste.`
+		} else {
+			input.nextElementSibling.classList.remove(`profile__reset-alert--visible`)
+		}
+	})
+}
+
+const passwordChecker = () => {
+    if(newPassInput.value == repeatPasswordInput.value && repeatPasswordInput.value !== ""){
+        newPassInput.value = ``
+		repeatPasswordInput.value = ``
+        repeatPassAlert.classList.remove(`profile__reset-alert--visible`)
+    } else if(newPassInput.value == repeatPasswordInput.value && repeatPasswordInput.value == "") {
+		repeatPassAlert.classList.add(`profile__reset-alert--visible`)
+		repeatPassAlert.textContent = `Podane pole nie może być puste.`
+	}
+	else {
+        repeatPassAlert.classList.add(`profile__reset-alert--visible`)
+		repeatPassAlert.textContent = 'Podane hasła nie są taki same.'
+    }
+}
+
+
+
+const checkPassInputs = () => {
+	checkValue()
+	passwordChecker()
+
+	let allertCount = 0
+
+	allAlerts.forEach(alert =>  {
+		if(alert.classList.contains(`profile__reset-alert--visible`)) {
+			allertCount++
+		} 
+	})
+
+	if(allertCount === 0) {
+		corectAllert.classList.add(`profile__reset-alert-corect--visible`)
+		allInputs.forEach(input => input.value = ``)
+	} else {
+		corectAllert.classList.remove(`profile__reset-alert-corect--visible`)
+	}
+}
+
+
+
+
+
+
 myProfileBtn.addEventListener(`click`, showMyProfile)
 editBtn.addEventListener(`click`, showEditPanel)
 historyBtn.addEventListener(`click`, showHistory)
 changePassBtn.addEventListener(`click`, showReset)
+
+resetPassbtn.addEventListener(`click`, checkPassInputs)
 
 navMobileBtn.addEventListener(`click`, handleNav);
 navLanguageBtn.addEventListener(`click`, handleLanguageNav);
